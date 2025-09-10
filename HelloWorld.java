0220273a2f3a7dcf101067b4b182b8fb80e31706
jrgustmadtown <jrgust.com@wisc.edu>
2025-09-10 16:55:31 -0500

    efficient

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        Scanner s = new Scanner(System.in);
-        int n = Integer.parseInt(s.nextLine());
-        
+        BufferedReader b = new BufferedReader(System.in);
+        int n = Integer.parseInt(br.readLine());
+       
         for (int i = 0; i < n; i++) 
         {
             String output = s.nextLine();
--- Makefile
+++ Makefile
@@ -1,4 +1,9 @@
+all: build run
+
 build:
 	javac HelloWorld.java
 run: 
 	java HelloWorld
+
+clean:
+	rm *.class
