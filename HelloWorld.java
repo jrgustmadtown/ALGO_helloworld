22800bc89092ff4c32ebc67809d2d508b44cf53e
jrgustmadtown <jrgust.com@wisc.edu>
2025-09-10 17:09:11 -0500

    fix all buffer stuff

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,15 @@
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-
-        BufferedReader b = new BufferedReader(System.in);
-        int n = Integer.parseInt(b.readLine());
-       
-        for (int i = 0; i < n; i++) 
-        {
-            String output = b.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(b.readLine());  // Read number of inputs
+        
+        for (int i = 0; i < n; i++) {
+            String output = b.readLine();  // Correct method to read line
             System.out.println("Hello, " + output + "!");
         }
     }
